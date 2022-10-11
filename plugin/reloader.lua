a33c9cf438d3ceedb045494918a3e7d993894fa0
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2022-10-11 15:10:52 +0300

    feat: add autocommands to reload configuration automatically

--- plugin/reloader.lua
+++ plugin/reloader.lua
@@ -3,3 +3,12 @@ vim.api.nvim_create_user_command(
 	require("reload-nvim").Reload,
 	{ desc = "Manually reload your neovim config" }
 )
+
+if vim.g.auto_reload_config then
+	local reloader_augroup = vim.api.nvim_create_augroup("reloader", { clear = true })
+	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		pattern = os.getenv("HOME") .. "/.config/nvim/*",
+		callback = require("reload-nvim").Reload,
+		group = reloader_augroup,
+	})
+end
